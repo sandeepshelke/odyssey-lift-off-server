0e9fa545622183917209c3a081380f914b2bfe34
Sandeep Shelke <sandeep@nano-frames.com>
2023-02-21 14:04:39 +0000

    move await into a function

--- src/index.js
+++ src/index.js
@@ -3,20 +3,24 @@ const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const TrackAPI = require("./datasources/track-api");
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    dataSources: () => {
-      return {
-        trackAPI: new TrackAPI()
-      };
-    },
-});
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      dataSources: () => {
+        return {
+          trackAPI: new TrackAPI()
+        };
+      },
+  });
 
-const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
+  const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
 
-console.log(`
-    🚀  Server is running!
-    🔉  Listening on port ${port}
-    📭  Query at ${url}
-`);
+  console.log(`
+      🚀  Server is running!
+      🔉  Listening on port ${port}
+      📭  Query at ${url}
+  `);
+}
+
+startApolloServer(typeDefs, resolvers);
